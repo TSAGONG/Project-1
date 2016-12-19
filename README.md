6670646abb5d32db768ab1c72335372716bbe99a
Tenzin Tsagong <tsagong@gmail.com>
2016-12-18 23:11:05 -0500

    snake

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Project-1
+# PROJECT 1 : SNAKE GAME 
+
+ A one player 2D game intended to challenge the user. There is only one level; therefore,
+ the speed is faster than what one would expect from the first level. Enjoy and get the 
+ highest score possibe. 
+
+![preview]('img/Screen Shot 2016-12-18')
+
+## WIREFRAMES:
+
+![wireframe1]('img/slack_for_ios_upload_720-1.jpg')
+![wireframe2]('slack_for_ios_upload_720.jpg')
+
+These were my early ideas in terms of how I wanted to approach making my game.
+Eventually, as I worked on my game I knew i would need more time to complete my ideas.
+Therefore, I only have one level instead of having a player advance to higher and more difficult levels after a player eats a certain amount of food. Due to this, I made sure to not make the game super easy so that people can be challenged and get the highest score possible. 
+
+## TECHNOLOGIES USED:
+
+-HTML
+-CSS
+-JAVASCRIPT
--- src/app.js
+++ src/app.js
@@ -1,11 +1,117 @@
 window.onload = function(){
 
-  const canvas = document.getElementById('gameBoard'), // retrives the node in the DOM
-  ctx = canvas.getContext('2d');  // this method accesses the drawing context
+var canvas = document.createElement('canvas');
+var ctx =  canvas.getContext('2d');
 
-  ctx.fillStyle = 'yellow';
-  ctx.fillRect(0, 0, 100, 100); //draws a filled rectangle at the X, Y position.
-  ctx.fill();
+canvas.width = 950;
+canvas.height = 700;
+
+ctx.font = "15px 'Press Start 2P', cursive";
+
+var body = document.getElementsByTagName('body')[0];
+body.appendChild(canvas);
+
+
+function init(){
+  direction = 'right';
+  score = 0;
+  body = [
+    {x: 100, y: 100},
+    {x:100, y: 100}
+  ];
+}
+
+init();
+
+function createFood(){
+  food = {
+    x: Math.floor(Math.random()*39),
+    y: Math.floor(Math.random()*24)};
+
+  for(var i=0; i<body.length; i++){
+    var snake = body[i];
+    if(food.x*20 == snake.x && food.y*20 == snake.y){
+    }
+  }
+}
+
+createFood();
+
+document.addEventListener('keydown', function(e){
+
+  if(e.keyCode === 37 && direction != 'right'){
+    direction = 'left';
+  }
+  else if(e.keyCode === 38 && direction != 'down'){
+    direction = 'up';
+  }
+  else if(e.keyCode === 39 && direction != 'left'){
+    direction = 'right';
+  }
+  else if(e.keyCode === 40 && direction != 'up'){
+    direction = 'down';
+  }
+});
+
+function eat(){
+  var lastBall = body[body.length-1];
+  if(direction == 'right'){
+     body.push({x:lastBall.x +20, y:lastBall.y})
+  }
+  if(direction == 'down'){
+     body.push({x:lastBall.x, y:lastBall.y +20})
+  }
+  if(direction == 'left'){
+     body.push({x:lastBall.x -20, y:lastBall.y})
+  }
+  if(direction == 'up'){
+     body.push({x:lastBall.x, y:lastBall.y -20})
+  }
+}
+
+/*function collission(){
+  if(body[0].x<0 || body[0].x>=)
+}*/
+
+function ani(){
+  ctx.clearRect(0, 0, 950, 700);
+  body.shift();
+  eat();
+  var lastBall = body[body.length-1];
+
+  if(lastBall.x == food.x*20 && lastBall.y == food.y*20 ){
+    score += 5;
+    eat();
+    createFood();
+  }
+
+  for(var i=0; i<body.length; i++){
+    snake = body[i];
+    if(i === body.length-1){
+      ctx.fillStyle = '#43FF00';
+    }
+
+  // This goes past the canvas and returns from the other side of the canvas.
+  /*  if(body.x >= 974 || body.x <= 974){
+      console.log('gameover');
+    }
+    if(body.y >=724 || body.y <= 724){
+      console.log('gameover');
+    }*/
+
+    if (snake.x ==lastBall.x && snake.y == lastBall.y && i < body.length-2){
+      alert('Game Over, Your Score Is ' + score);
+      init();
+    }
+
+    ctx.fillRect(snake.x, snake.y, 19, 19);
+  }
+  ctx.fillRect(food.x*20, food.y*20, 19, 19);
+
+  ctx.fillText('Score: ' + score , 10, canvas.height-10);
+}
+
+setInterval(ani, 60);
 
 }
 
